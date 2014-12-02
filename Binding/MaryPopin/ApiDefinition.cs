38bf3e3ba140d428a2b28c6ce567139c527d68e9
Philippe Bernery <philippe.bernery@backelite.com>
2014-12-02 18:00:15 +0100

    Full binding, but does not compile...

---
+++ Binding/MaryPopin/ApiDefinition.cs
@@ -0,0 +1,183 @@
+﻿using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MaryPopin
+{
+	// The first step to creating a binding is to add your native library ("libNativeLibrary.a")
+	// to the project by right-clicking (or Control-clicking) the folder containing this source
+	// file and clicking "Add files..." and then simply select the native library (or libraries)
+	// that you want to bind.
+	//
+	// When you do that, you'll notice that MonoDevelop generates a code-behind file for each
+	// native library which will contain a [LinkWith] attribute. MonoDevelop auto-detects the
+	// architectures that the native library supports and fills in that information for you,
+	// however, it cannot auto-detect any Frameworks or other system libraries that the
+	// native library may depend on, so you'll need to fill in that information yourself.
+	//
+	// Once you've done that, you're ready to move on to binding the API...
+	//
+	//
+	// Here is where you'd define your API definition for the native Objective-C library.
+	//
+	// For example, to bind the following Objective-C class:
+	//
+	//     @interface Widget : NSObject {
+	//     }
+	//
+	// The C# binding would look like this:
+	//
+	//     [BaseType (typeof (NSObject))]
+	//     interface Widget {
+	//     }
+	//
+	// To bind Objective-C properties, such as:
+	//
+	//     @property (nonatomic, readwrite, assign) CGPoint center;
+	//
+	// You would add a property definition in the C# interface like so:
+	//
+	//     [Export ("center")]
+	//     PointF Center { get; set; }
+	//
+	// To bind an Objective-C method, such as:
+	//
+	//     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
+	//
+	// You would add a method definition to the C# interface like so:
+	//
+	//     [Export ("doSomething:atIndex:")]
+	//     void DoSomething (NSObject object, int index);
+	//
+	// Objective-C "constructors" such as:
+	//
+	//     -(id)initWithElmo:(ElmoMuppet *)elmo;
+	//
+	// Can be bound as:
+	//
+	//     [Export ("initWithElmo:")]
+	//     IntPtr Constructor (ElmoMuppet elmo);
+	//
+	// For more information, see http://docs.xamarin.com/ios/advanced_topics/binding_objective-c_libraries
+	//
+
+	[BaseType(typeof(UIViewController))]
+	[Category]
+	interface MaryPopinUIViewControllerExtension {
+		#region Methods
+
+		/// <summary>
+		/// Present a popin controller as a child of the receiver, centered inside an arbitrary rect.
+		/// </summary>
+		/// <param name="popinController">The controller to present as a popin.</param>
+		/// <param name="animated">Pass `YES` to animate the presentation. Otherwise, pass `NO`.</param>
+		/// <param name="completion">A completion handler, or `NULL`</param>
+		[Export("presentPopinController:animated:completion:")]
+		void PresentPopinController(UIViewController popinController, bool animated, [NullAllowed] CompletionDelegate completion);
+
+		/// <summary>
+		/// Present a popin controller as a child of the receiver, centered inside an arbitrary rect.
+		/// </summary>
+		/// <param name="popinController">The controller to present as a popin.</param>
+		/// <param name="fromRect">An arbitrary rect in which the popin should be centered.</param>
+		/// <param name="animated">Pass `YES` to animate the presentation. Otherwise, pass `NO`.</param>
+		/// <param name="completion">A completion handler, or `NULL`.</param>
+		[Export("presentPopinController:fromRect:animated:completion:")]
+		void PresentPopinController(UIViewController popinController, RectangleF fromRect, bool animated, [NullAllowed] CompletionDelegate completion);
+
+		/// <summary>
+		/// Dismiss the visible popin if any.
+		/// </summary>
+		/// <param name="animated">Pass `YES` to animate the dismiss. Otherwise, pass `NO`.</param>
+		[Export("dismissCurrentPopinControllerAnimated:")]
+		void DismissCurrentPopinController(bool animated);
+
+		/// <summary>
+		/// Dismiss the visible popin if any.
+		/// </summary>
+		/// <param name="animated">Pass `YES` to animate the dismiss. Otherwise, pass `NO`.</param>
+		/// <param name="completion">A completion handler, or `NULL`.</param>
+		[Export("dismissCurrentPopinControllerAnimated:completion:")]
+		void DismissCurrentPopinController(bool animated, [NullAllowed] CompletionDelegate completion);
+
+		#endregion
+
+
+		#region Properties
+
+		/// <summary>
+		/// A reference to the popin presented as a child controller.
+		/// </summary>
+		/// <value>The controller presented as a popin or `nil`.</value>
+		[Export("presentedPopinViewController")]
+		UIViewController PresentedPopinViewController { get; }
+
+		/// <summary>
+		/// A reference to the parent presenting the popin.
+		/// </summary>
+		/// <value>The controller presenting the popin, or `nil`.</value>
+		[Export("presentingPopinViewController")]
+		UIViewController PresentingPopinViewController{ get; }
+
+		/// <summary>
+		/// Gets or sets the size of the prefered popin content.
+		/// This value may not be respected if popin is bigger than the presenting controller view.
+		/// If not set, the default size will be the controller view size.
+		/// </summary>
+		/// <value>The size of the prefered popin content.</value>
+		[Export("preferedPopinContentSize")]
+		SizeF PreferedPopinContentSize{ get; set; }
+
+		/// <summary>
+		/// The transition style to use when presenting a popin. Default value is `BKTPopinTransitionStyleSlide`.
+		/// For a list of possible transition style, see `BKTPopinTransitionStyle`.
+		/// </summary>
+		/// <value>The popin transition style.</value>
+		[Export("popinTransitionStyle")]
+		BKTPopinTransitionStyle PopinTransitionStyle { get; set; }
+
+		/// <summary>
+		/// The transition direction to use when presenting a popin. Default value is `BKTPopinTransitionDirectionBottom`.
+		/// </summary>
+		/// <value>The popin transition direction.</value>
+		[Export("popinTransitionDirection")]
+		BKTPopinTransitionDirection PopinTransitionDirection { get; set; }
+
+		/// <summary>
+		/// The options to apply to the popin. For a list of possible options, see BKTPopinOption.
+		/// </summary>
+		/// <value>The popin options.</value>
+		[Export("popinOption")]
+		BKTPopinOption PopinOptions { get; set; }
+
+		/// <summary>
+		/// Get the custom in animation block. Default value is nil.
+		/// </summary>
+		/// <value>The popin custom in animation.</value>
+		[Export("popinCustomInAnimation")]
+		AnimationDelegate PopinCustomInAnimation { get; set; }
+
+		/// <summary>
+		/// Get the custom out animation block. Default value is nil.
+		/// </summary>
+		/// <value>The popin custom out animation.</value>
+		[Export("popinCustomOutAnimation")]
+		AnimationDelegate PopinCustomOutAnimation { get; set; }
+
+		/// <summary>
+		/// The options to apply to the popin. Default value is `BKTPopinAlignementOptionCentered`.
+		/// </summary>
+		/// <value>The popin alignment.</value>
+		[Export("popinAlignment")]
+		BKTPopinAlignementOption PopinAlignment { get; set; }
+
+		/// <summary>
+		/// An object used to configure the blurred background.
+		/// </summary>
+		/// <value>The blur parameters.</value>
+		[Export("blurParameters"), NullAllowed]
+		BKTBlurParameters BlurParameters { get; set; }
+
+		#endregion
+	}
+}
---
+++ Binding/MaryPopin/StructsAndEnums.cs
@@ -0,0 +1,130 @@
+﻿using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MaryPopin
+{
+	[BaseType(typeof(NSObject))]
+	interface BKTBlurParameters
+	{
+		[Export("alpha")]
+		float Alpha { get; set; }
+
+		[Export("radius")]
+		float Radius { get; set; }
+
+		[Export("saturationDeltaFactor")]
+		float SaturationDeltaFactor { get; set; }
+
+		[Export("tintColor")]
+		UIColor TintColor { get; set; }
+	}
+
+	delegate void CompletionDelegate();
+
+	delegate void AnimationDelegate(UIViewController popinController, RectangleF initialFrame, RectangleF finalFrame);
+
+	public enum BKTPopinTransitionStyle
+	{
+		/// <summary>
+		/// When the view controller is presented, its view slide in the parent view controller and slide out on dismiss.
+		/// </summary>
+		Slide,
+		/// <summary>
+		/// When the view controller is presented, its view fade in and fade out on dismiss. Transition direction is ignored for this kind of transition.
+		/// </summary>
+		CrossDissolve,
+		/// <summary>
+		/// When the view controller is presented, its view fade in with a zoom out effect and fade out with a zoom in effect on dismiss. Transition direction is ignored for this kind of transition.
+		/// </summary>
+		Zoom,
+		/// <summary>
+		/// When the view controller is presented, its view slide in with a bounce effect and slide out on dismiss.
+		/// </summary>
+		SpringySlide,
+		/// <summary>
+		/// When the view controller is presented, its view zoom and fade in with a bounce effect. Transition direction is ignored for this kind of transition.
+		/// </summary>
+		SpringyZoom,
+		/// <summary>
+		/// When the view controller is presented, its view has a undefined behavior.
+		/// </summary>
+		Snap,
+		/// <summary>
+		/// When the view controller is presented, its view has a custom animation.
+		/// </summary>
+		Custom
+	}
+
+	public enum BKTPopinTransitionDirection
+	{
+		/// <summary>
+		/// Presentation transition will start from the bottom of the parent view. Respectively, dismiss transition will end to the bottom of the parent view.
+		/// </summary>
+		Bottom,
+		/// <summary>
+		/// Presentation transition will start from the top of the parent view. Respectively, dismiss transition will end to the top of the parent view.
+		/// </summary>
+		Top,
+		/// <summary>
+		/// Presentation transition will start from the left of the parent view. Respectively, dismiss transition will end to the left of the parent view.
+		/// </summary>
+		Left,
+		/// <summary>
+		/// Presentation transition will start from the right of the parent view. Respectively, dismiss transition will end to the right of the parent view.
+		/// </summary>
+		Right
+	}
+
+	public enum BKTPopinOption
+	{
+		/// <summary>
+		/// Default behaviour.
+		/// </summary>
+		Default = 0,
+		/// <summary>
+		/// Disable popin reaction to keyboard notifications.
+		/// </summary>
+		IgnoreKeyboardNotification = 1 << 0,
+		/// <summary>
+		/// Disable auto dismiss when touching outside of the popin view.
+		/// </summary>
+		DisableAutoDismiss = 1 << 1,
+		/// <summary>
+		/// Takes a screenshot of presenting view, blurs it and uses it as dimming view. Available only on ios 7.x.
+		/// </summary>
+		BlurryDimmingView = 1 << 2,
+		/// <summary>
+		/// Disable parallax effect on iOS7.
+		/// </summary>
+		DisableParallaxEffect = 1 << 3,
+		/// <summary>
+		/// Set a background dimming view with a clear color. Default is a semi-transparent black background.
+		/// </summary>
+		DimmingViewStyleNone = 1 << 16
+	}
+
+	public enum BKTPopinAlignementOption
+	{
+		/// <summary>
+		/// Popin will be centered in container. Default.
+		/// </summary>
+		Centered,
+		/// <summary>
+		/// Popin will be stuck to top in container.
+		/// </summary>
+		Up,
+		/// <summary>
+		/// Popin will be left-aligned in container.
+		/// </summary>
+		Left,
+		/// <summary>
+		/// Default will be stuck to bottom in container.
+		/// </summary>
+		Down,
+		/// <summary>
+		/// Popin will be right-aligned in container.
+		/// </summary>
+		Right
+	}
+}
